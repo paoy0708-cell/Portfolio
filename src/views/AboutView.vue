10b655ecacd8aec417b3451a95c9848f5ba1b844
Akira Uchiyama <pao.y0708@gmail.com>
2026-07-09 11:47:01 -0700

    second push

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -39,8 +39,7 @@ export default {
         {
           id: 6,
           name: "Codeigniter",
-          imageUrl:
-            "https://toppng.com/free-image/code-igniter-logo-PNG-free-PNG-Images_472578",
+          imageUrl: "https://www.codeigniter.com/assets/icons/ci-logo.png",
           status: "Advance",
         },
         {
@@ -89,57 +88,151 @@ export default {
           id: 13,
           name: "Python",
           imageUrl:
-            "https://cdn4.iconfinder.com/data/icons/logos-3/600/python_logo-512.png",
+            "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png",
           status: "Advance",
         },
         {
           id: 14,
-          name: "AI",
+          name: "DeepLearning",
           imageUrl:
-            "https://cdn4.iconfinder.com/data/icons/logos-3/600/ai_logo-512.png",
+            "https://ai-dev.deeplearning.ai/images/sf/assets/deeplearning-logo.webp",
           status: "Advance",
         },
         {
           id: 15,
           name: "Ruby",
-          imageUrl:
-            "https://cdn4.iconfinder.com/data/icons/logos-3/600/ruby_logo-512.png",
+          imageUrl: "https://www.ruby-lang.org/images/header-ruby-logo.svg",
           status: "Advance",
         },
         {
           id: 16,
           name: "C++",
           imageUrl:
-            "https://cdn4.iconfinder.com/data/icons/logos-3/600/c-512.png",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/250px-ISO_C%2B%2B_Logo.svg.png",
           status: "Advance",
         },
         {
           id: 17,
           name: "C#",
           imageUrl:
-            "https://cdn4.iconfinder.com/data/icons/logos-3/600/c-512.png",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Logo_C_sharp.svg/330px-Logo_C_sharp.svg.png?_=20221121173824",
           status: "Advance",
         },
         {
           id: 18,
           name: ".NET",
           imageUrl:
-            "https://cdn4.iconfinder.com/data/icons/logos-3/600/net-512.png",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Microsoft_.NET_logo.svg/500px-Microsoft_.NET_logo.svg.png?_=20200524040737",
           status: "Advance",
         },
         {
           id: 19,
           name: "Angular",
           imageUrl:
-            "https://cdn4.iconfinder.com/data/icons/logos-3/600/angular-512.png",
+            "https://angular.dev/assets/images/press-kit/angular_icon_gradient.gif",
           status: "Advance",
         },
         {
           id: 20,
-          name: "Mobile",
+          name: "Android",
           imageUrl:
-            "https://cdn4.iconfinder.com/data/icons/logos-3/600/mobile-512.png",
-          status: "intermediate",
+            "https://developer.android.com/static/images/brand/android-head_flat.png",
+          status: "Advance",
+        },
+        {
+          id: 21,
+          name: "Next.js",
+          imageUrl:
+            "https://icon.icepanel.io/Technology/png-shadow-512/Next.js.png",
+          status: "Advance",
+        },
+        {
+          id: 22,
+          name: "OpenAI",
+          imageUrl:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/OpenAI_logo_2025_%28symbol%29.svg/120px-OpenAI_logo_2025_%28symbol%29.svg.png",
+          status: "Advance",
+        },
+        {
+          id: 23,
+          name: "MachineLearning",
+          imageUrl:
+            "https://upload.wikimedia.org/wikipedia/commons/d/d5/Hey_Machine_Learning_Logo.png?_=20180611150542",
+          status: "Advance",
+        },
+        {
+          id: 24,
+          name: "Java",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Java.svg",
+          status: "Advance",
+        },
+        {
+          id: 25,
+          name: "Flutter",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Flutter.svg",
+          status: "Advance",
+        },
+        {
+          id: 25,
+          name: "GraphQL",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/GraphQL.svg",
+          status: "Advance",
+        },
+        {
+          id: 26,
+          name: "Electron",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Electron.svg",
+          status: "Advance",
+        },
+        {
+          id: 27,
+          name: "Go",
+          imageUrl: "https://go.dev/images/go-logo-white.svg",
+          status: "Advance",
+        },
+        {
+          id: 28,
+          name: "Spring",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Spring.svg",
+          status: "Advance",
+        },
+        {
+          id: 29,
+          name: "Keras",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Keras.svg",
+          status: "Advance",
+        },
+        {
+          id: 30,
+          name: "Django",
+          imageUrl:
+            "https://icon.icepanel.io/Technology/png-shadow-512/Django.png",
+          status: "Advance",
+        },
+        {
+          id: 31,
+          name: "Flask",
+          imageUrl:
+            "https://icon.icepanel.io/Technology/png-shadow-512/Flask.png",
+          status: "Advance",
+        },
+        {
+          id: 32,
+          name: "FastAPI",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/FastAPI.svg",
+          status: "Advance",
+        },
+        {
+          id: 33,
+          name: "Nest.js",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Nest.js.svg",
+          status: "Advance",
+        },
+        {
+          id: 34,
+          name: "TypeScript",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/TypeScript.svg",
+          status: "Advance",
         },
       ],
       tools: [
@@ -168,6 +261,73 @@ export default {
           name: "MySQL",
           imageUrl: "https://cdn-icons-png.flaticon.com/512/5968/5968313.png",
           status: "Database",
+        },
+        {
+          id: 5,
+          name: "MongoDB",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/MongoDB.svg",
+          status: "Advance",
+        },
+        {
+          id: 6,
+          name: "Firebase",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Firebase.svg",
+          status: "Advance",
+        },
+        {
+          id: 7,
+          name: "Jenkins",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/TypeScript.svg",
+          status: "Advance",
+        },
+        {
+          id: 8,
+          name: "AWS",
+          imageUrl:
+            "https://icon.icepanel.io/Technology/png-shadow-512/AWS.png",
+          status: "Advance",
+        },
+        {
+          id: 9,
+          name: "Azure",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Azure.svg",
+          status: "Intermediate",
+        },
+        {
+          id: 10,
+          name: "GoogleCloud",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Google-Cloud.svg",
+          status: "Advance",
+        },
+        {
+          id: 11,
+          name: "BitBucket",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/BitBucket.svg",
+          status: "Advance",
+        },
+        {
+          id: 12,
+          name: "DegitalOcean",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Digital-Ocean.svg",
+          status: "Advance",
+        },
+        {
+          id: 13,
+          name: "Docker",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Docker.svg",
+          status: "Advance",
+        },
+        {
+          id: 14,
+          name: "PostgreSQL",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/PostgresSQL.svg",
+          status: "Advance",
+        },
+        {
+          id: 15,
+          name: "Postman",
+          imageUrl: "https://icon.icepanel.io/Technology/svg/Postman.svg",
+          status: "Advance",
         },
       ],
     };
@@ -204,30 +364,23 @@ export default {
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
-            &nbsp; &nbsp; &nbsp; Hello everyone! My name is Akira Uchiyama. I am
-            a Full-Stack and AI/ML Engineer with 10+ years of experience
+            Hello everyone! <br />
+            I am Akira Uchiyama. <br />
+            I am a Full-Stack and AI/ML Engineer with 10+ years of experience
             designing and building scalable, cloud-native software solutions, as
             well as developing state-of-the-art LLM, Generative AI, and NLP
-            systems.
-          </p>
-          <p class="mb-3 fadein-left fadeins-2">
-            &nbsp; &nbsp; &nbsp; I have a proven track record of collaborating
+            systems. &nbsp; <br />I have a proven track record of collaborating
             with cross-functional teams to deliver production-grade
             applications, including microservices architectures, CI/CD
             pipelines, and real-time data platforms using technologies such as
-            Django, React, Kubernetes, and Terraform.
-          </p>
-          <p class="mb-3 fadein-left fadeins-2">
-            &nbsp; &nbsp; &nbsp; My expertise includes integrating RAG
-            architectures, LangChain, Hugging Face, GPT-3.5/4, and reinforcement
-            learning techniques to optimize system performance, reduce
-            operational costs, and enhance end-user experiences.
-          </p>
-          <p class="mb-3 fadein-left fadeins-2">
-            &nbsp; &nbsp; &nbsp; I am passionate about building end-to-end AI
-            platforms that combine modern web technologies, deep learning, and
-            robust DevOps practices to create impactful solutions for both
-            startups and enterprise environments.
+            Django, React, Kubernetes, and Terraform. &nbsp; <br />
+            My expertise includes integrating RAG architectures, LangChain,
+            Hugging Face, GPT-3.5/4, and reinforcement learning techniques to
+            optimize system performance, reduce operational costs, and enhance
+            end-user experiences. &nbsp; <br />I am passionate about building
+            end-to-end AI platforms that combine modern web technologies, deep
+            learning, and robust DevOps practices to create impactful solutions
+            for both startups and enterprise environments.
           </p>
         </div>
       </section>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,78 +1,100 @@
 <template>
   <div class="container mx-auto p-3 md:p-8">
     <div class="flex flex-col-reverse md:flex-row relative">
-      <div class="w-full md:w-2/3">
+      <div class="w-full">
         <div class="flex flex-col gap-4 md:px-20 fade-zoom-up">
           <article v-for="article in articles" :key="article.id">
-            <router-link :to="`/read/${article.slug}/${article.id}`" class="flex w-full bg-[#1e1e1f] border-[#383838] rounded-xl text-left text-white p-5 md:py-7 md:px-8 cursor-pointer hover:bg-[#282828] items-center">
-              <div class="w-full pr-4">
-                <div class="text-xs mb-1 text-slate-400 flex items-center italic">
-                  <div class="h-[1px] w-20 bg-amber-200 md:w-5 aos-init aos-animate mr-2"></div> {{ article.date }}
+            <router-link
+              :to="`/read/${article.slug}/${article.id}`"
+              class="flex w-full bg-[#1e1e1f] border-[#383838] rounded-xl text-left text-white p-5 md:py-7 md:px-8 cursor-pointer hover:bg-[#282828] items-center"
+            >
+              <div class="w-full pr-4 bg-[#1e1e1f] border-[#383838]">
+                <div
+                  class="text-xs mb-1 text-slate-400 flex items-center italic"
+                >
+                  <div
+                    class="h-[1px] w-20 bg-amber-200 md:w-5 aos-init aos-animate mr-2"
+                  ></div>
+                  {{ article.date }}
                 </div>
-                <h1 class="text-sm md:text-md text-amber-200 font-bold mb-2 paraf">{{ article.title }}</h1>
-                <div class="text-sm hidden md:block paraf">{{ article.desc }}</div>
-              </div>
-              <div>
-                <div class="w-20 h-20 md:w-28 flex items-center md:h-28">
-                  <img :src="article.image" class="rounded-lg md:rounded-xl" alt="">
+                <h1
+                  class="text-sm md:text-md text-amber-200 font-bold mb-2 paraf"
+                >
+                  {{ article.title }}
+                </h1>
+                <div class="text-sm text-amber-200 md:block paraf">
+                  {{ article.desc }}
+                </div>
+                <div class="text-sm hidden md:block paraf">
+                  {{ article.skills }}
                 </div>
               </div>
             </router-link>
           </article>
         </div>
       </div>
-      <div class="w-full md:w-1/3 h-fit p-8 md:sticky md:top-24">
-        <!-- Sidebar -->
-        <div class="flex flex-col text-left">
-          <div class="bg-clip-text bg-gradient-to-r from-slate-100 to-amber-300 text-transparent">Let's share experiences,
-            stories, and knowledge together.
-          </div>
-          <div class="h-[1px] mt-7 mb-7 w-20 bg-amber-200 aos-init aos-animate mr-2"></div>
-          <div class="hidden md:block">
-            <div class="text-white text-md font-semibold">Topics</div>
-            <div class="mt-3 flex flex-wrap gap-1">
-              <span
-                class="py-2 px-3 rounded-2xl bg-[#1e1e1f] hover:bg-white/20 text-white text-xs cursor-pointer">NodeJS</span>
-              <span
-                class="py-2 px-3 rounded-2xl bg-[#1e1e1f] hover:bg-white/20 text-white text-xs cursor-pointer">Technology</span>
-            </div>
-            <!-- <div class="h-[1px] mt-7 mb-7 w-20 bg-amber-200 aos-init aos-animate mr-2"></div>
-            <div class="text-white text-md font-semibold">Popular Articles</div> -->
-
-          </div>
-        </div>
-      </div>
     </div>
   </div>
   <ArticleList />
 </template>
-  
+
 <script>
-import ArticleList from '@/components/ArticleList.vue';
-import axios from "axios";
+import ArticleList from "@/components/ArticleList.vue";
 export default {
   data() {
     return {
-      articles: []
-    }
+      articles: [
+        {
+          title: "AI Engineer",
+          desc: "Listen Labs (Startup),San Francisco, California",
+          slug: "aspernatur-eos-perspiciatis",
+          date: " Aug 2023 - June 2025",
+          skills:
+            "Hugging Face, Transformers, PU learning, Natural language processing, NLTK, spacy, topic modelling, Sentiment analysis, Word Embedding, scikit-learn, Spring Boot, Django, React, Next.js, PostgreSQL, RPC, gRPC, AWS, GCP, SageMaker, Glue, ETL, PU learning",
+          content:
+            "•Redesigned and optimized data pipelines to unify subscription KPIs across multiple streaming platforms, collaborating with domain experts to refine data models and analytics frameworks.<br/>•Developed Python-based ETL APIs, reducing overall processing time by 30% on AWS-based infrastructure.<br/>•Led a team of 5 software engineers to build and deploy an end-to-end heart condition detection system that achieves 94.27% accuracy; it records heart sounds with a simple stethoscope and analyzes the clips in Keras and TensorFlow.<br/>•Designed a scalable architecture with a real-time NLP engine, content management system, analytics dashboard, and one-click deployment module.<br/>•Trained deep neural nets on the cleaned clips, which raised accuracy and cut false positives by 20%.<br/>•Drove another 15% lift by pairing those networks with SVM, KNN, and trees, then tuning them on over sampled data through grid search.<br/>•Leveraged Databricks for distributed data processing and collaborative development, reducing data pipeline latency, and accelerating iteration speed by approximately 40% in the ML lifecycle.<br/>•Developed a machine learning model to identify high-propensity prescribers (HCPs) for a newly launched oncology drug, leveraging Positive-Unlabeled (PU) learning techniques to handle data imbalance.<br/>•Designed and trained a GENPU GAN-based model, improving precision and recall for more effective targeting. Automated model retraining using AWS SageMaker, Glue, and Redshift, integrating real-time insights into an nteractive dashboard for sales teams.<br/>•Integrated machine learning inference services into a Python, Java backend, enabling real-time predictions delivered to a Javascript frontend.<br/>•Optimized API design and data serialization to minimize latency and support scalable user interactions.",
+          id: "1",
+        },
+        {
+          title: "AI Engineer",
+          desc: "Google, San Jose, CA",
+          slug: "neque-dicta-eaque",
+          date: "Jan 2021 - July 2023",
+          skills:
+            "AWS, Kinesis, Glue, DynamoDB, and QuickSight, Hugging Face, Transformers, OpenAI,  Natural language processing, NLTK, RAG, Pytorch, Tensorflow, CNN, spacy, scikit-learn, Nvidia NeMo,ANs, PU learning, LLMs, ETL, Spring   Boot, Django, GoLang, React, Vue, PostgreSQL, RPC, gRPC,  CI/CD, GCP, Azure, Redshift",
+          content:
+            "•Developed and deployed a Conversational AI chatbot using Nvidia NeMo and AWS, integrating speech-to-text models to enhance natural language understanding.<br/>•Leveraging LangChain,I effectively manage interactions with language models, orchestrate omponents across the workflow, and integrate external resources and databases with ease and efficiency.<br/>•Optimized the pipeline to reduce latency by 35% and improve response accuracy.<br/>•Architected and productionized an end-to-end Conversational AI system leveraging AWS, Kinesis, Glue, DynamoDB, and QuickSight,  enabling real-time customer interactions and analytics.<br/>•Developed an automated age-based diagnostic system for lung inflammation detection using real X-ray images, leveraging advanced image processing techniques, segmentation methods, and deep learning models. ine-tuned d trained a custom CNN model, optimizing feature extraction and classification accuracy for improved medical diagnosis.<br/>•Steered product management for 'Compliance AI CoPilot' in developing harmonious design, resolution of issues, and feature prioritization for enterprise applications of generative AI.<br/>•Refined and modernized the extraction of meta-data entities by applying few-shot learning, in-context learning, and recursive reasoning techniques.<br/>•Engineered AI frameworks using cutting-edge LLMs, and retrieval-augmented generation for enterprise applications.<br/>• Acquired experience in Azure, OpenAI, and advanced AI concepts, including vector indexing, data ingestion, and entity categorization.<br>•Designed a scalable architecture with a real-time NLP engine, Hugging ce, content management system, analytics dashboard, and one-click deployment module.<br/>•Integrated machine learning and NLP models into a Go, Python backend, exposing scalable inference APIs consumed by Javascript frontends; streamlined data flow and optimized API design to reduce latency and enhance user experience.<br/>•Designed and deployed end-to-end model serving pipelines, connecting real-time ML predictions with web applications; leveraged cloud services and CI/CD practices to enable seamless updates and ensure high availability in production environments.",
+          id: "2",
+        },
+        {
+          title: "Machine Learning Research Intern",
+          desc: "Google, San Jose, CA",
+          date: " Jan 2020 - Dec 2020",
+          skills:
+            "Hugging Face, Transformers, OpenAI,  RAG, Pytorch, Tensorflow, CNN, Natural language processing, NLTK,scikit-learn, LLMs, ETL, AWS",
+          slug: "cumque-error-sapiente",
+          content:
+            "•Conducted in-depth study and research on Deep Learning and Neural Networks, focusing on state-of-the-art methodologies and techniques in the field.<br/>•Executed hands-on experimentation with various Deep Learning frameworks and libraries, including TensorFlow, PyTorch and Keras, to gain practical insights into model development and optimization.<br/>•Applied theoretical knowledge to design and implement a basic Convolutional Neural etwork (CNN) architecture tailored for recognizing handwritten digits.<br/>•Utilized Python programming language along with relevant libraries such as NumPy and Matplotlib to preprocess datasets and visualize model performance metrics.<br/>•Implemented data augmentation techniques to enhance the robustness of the CNN model and improve its generalization ability across diverse handwriting styles.<br/>•Fine-tuned hyperparameters and conducted systematic model validation to optimize the performance of the CNN, achieving notable accuracy in digit recognition tasks.<br/>•Collaborated with senior researchers and engineers to discuss findings, troubleshoot challenges, and refine the architecture of the CNN model for improved accuracy and efficiency.<br/>•Documented the entire development process, including model architecture, training procedures, and experimental results, in a comprehensive report for knowledge sharing and future reference.<br/>•Presented research findings and project outcomes to internal stakeholders, showcasing the effectiveness and potential applications of the developed CNN model in real-world scenarios.",
+          id: "3",
+        },
+        {
+          title: "Full-Stack & AI Engineer",
+          desc: "OpenSpace (Startup), San Francisco, CA",
+          date: "Jul 2017 - Dec 2019",
+          skills:
+            "React, Angular, Nest.js, Next.js, .NET, AWS, Ruby on Rails, Python ,Hugging Face, Transformers, OpenAI,  Natural language processing, NLTK, RAG, Pytorch, Tensorflow, CNN, spacy, scikit-learn, Nvidia NeMo,ANs, PU learning, LLMs, ETL, Spring   Boot, Django, GoLang,",
+          slug: "ut-ad-consequuntur",
+          content:
+            "•Built ML infrastructure with Golang, Docker, Databricks, Kubernetes.<br/>•Developed backend logic for LLM pipelines with PyTorch, Spark, Pandas.<br/>•Added LLM safety features using Hugging Face.<br/>•Deployed using blue-green deployments and rolling updates in EKS.<br/>•Built monitoring dashboards with Grafana and Prometheus.<br/>•Managed secrets with AWS Secrets Manager.<br/>•Tuned auto-scaling, cutting infra costs by ~20%.<br/>•Automated testing with NUnit, Selenium.<br/>•Monitored systems with AWS CloudWatch, Prometheus alerts.<br/>•Automated backup & recovery with AWS Backup, RDS snapshots.<br/>•Stored documents securely on AWS S3 (HIPAA compliance).<br/>•Developed scalable backend services in Node.js, PHP, Django, FastAPI, Flask; managed 1M+ monthly API requests.<br/>•Built dashboards with React and Next.js, improving client decision-making speed by 25%.<br/>•Automated CI/CD using GitLab and Jenkins, improving code coverage and cutting deployment errors by 35%.<br/>•Enhanced Kubernetes logging and alert systems, improving stability by 40%.<br/>•Developed and maintained enterprise healthcare web applications for lab workflow automation, digital client portals, and online reporting - using Django, Flask, and SQL Server for backend systems.<br/>•Designed and optimized SQL Server stored procedures, indexing strategies, and data models to support high-volume transactional processing of lab orders, test results, and reporting pipelines.<br/>•Delivered Single Page Applications (SPA) with AngularJS and jQuery, enhancing internal lab staff productivity and improving test result turnaround times.<br/>•Maintained and extended legacy WCF services and SOAP APIs for integration with EMR systems and external lab partners, applying WS-Security and HIPAA-compliant encryption and data handling.<br/>•Integrated RabbitMQ with Node.js services to support real-time processing of lab order events,result notifications, and system alerts across multiple applications.<br/>•Developed reusable front-end components and shared UI libraries to standardize presentation across client and internal lab applications.<br/>•Wrote automated unit tests and integration tests using NUnit, contributing to regression prevention and improving release quality.<br/>•Participated in Agile delivery cycles, working closely with business stakeholders and QA to deliver iterative enhancements to critical lab applications.<br/>•Supported CI/CD pipelines using Team Foundation Server (TFS) and MSBuild, improving deployment consistency and reducing cycle times for production releases.<br/>•Built application monitoring and centralized logging with ELK Stack (Elasticsearch, Logstash,Kibana), enhancing production support and troubleshooting capabilities.",
+          id: "4",
+        },
+      ],
+    };
   },
   components: {
-    ArticleList
+    ArticleList,
   },
-  mounted() {
-    this.getArticles();
-  },
-  methods: {
-    async getArticles() {
-      axios.get('https://64a38c9cc3b509573b564183.mockapi.io/api/blog/all')
-        .then(response => {
-          this.articles = response.data;
-        })
-    },
-
-  }
-}
-
+};
 </script>
 
 <style scoped>
@@ -82,7 +104,7 @@ export default {
   text-overflow: ellipsis;
   overflow: hidden;
 }
-@media (min-width: 768px) { 
+@media (min-width: 768px) {
   .paraf {
     display: -webkit-box;
   }
